dea3e38e1f73a3632804945b76942b226618db03
Gérald Barré <meziantou@users.noreply.github.com>
2025-04-15 16:42:34 -0400

    wip

--- Build.ps1
+++ Build.ps1
@@ -11,7 +11,7 @@ Process {
             throw ("An error occurred while executing command: {0}" -f $Command)
         }
     }
-    
+
     $workingDir = Join-Path $PSScriptRoot "src"
     $outputDir = Join-Path $PSScriptRoot ".output"
     $nupkgsPath = Join-Path $outputDir "*.nupkg"
@@ -19,7 +19,7 @@ Process {
     try {
         Push-Location $workingDir
         Remove-Item $outputDir -Force -Recurse -ErrorAction SilentlyContinue
-    
+
         Exec { & dotnet clean -c Release }
         Exec { & dotnet build -c Release }
         Exec { & dotnet test  -c Release --results-directory "$outputDir" --no-restore -l "trx" -l "console;verbosity=detailed" }
